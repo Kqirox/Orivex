4f39b93c287010bb7b3f3849adab9e22640fdf43
Martin Obe <obemartin40@gmail.com>
2026-04-28 09:53:37 -0700

    Added Footer to App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from "@/components/landing/hero";
 import WhyLearnault from "@/components/landing/why-learnault";
 import Testimonial from "@/components/landing/testimonial";
-
+import Footer from "@/components/landing/footer";
 const Home = () => {
   return (
     <main>
       <Hero />
       <WhyLearnault />
       <Testimonial />
+      <Footer/>
     </main>
   );
 };

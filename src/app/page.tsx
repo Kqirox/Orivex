9391aed0f7052693ddd69b466b64d24be8e4f812
luther10293-a11y <luther10293@gmail.com>
2026-04-28 13:42:02 +0100

    #138 [Frontend] Build the UI of the Testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/landing/hero";
 import WhyLearnault from "@/components/landing/why-learnault";
+import Testimonial from "@/components/landing/testimonial";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <WhyLearnault />
+      <Testimonial />
     </main>
   );
 };

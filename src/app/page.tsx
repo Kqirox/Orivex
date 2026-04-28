3f75c904c65cf3f6c23c12023e15c8e3d26b9859
Samuel Ajayi <samuelajayi554@gmail.com>
2026-04-28 12:08:43 +0100

    Added why Orivex landing section, both desktop and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing/hero";
+import WhyLearnault from "@/components/landing/why-learnault";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <WhyLearnault />
     </main>
   );
 };
